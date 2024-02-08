9ec39359c085ee78467c6f48aac8796c9bcf586f
Svana Björg <100528784+SvanaBirgis@users.noreply.github.com>
2024-02-08 13:56:34 +0000

    Attempt to fix failed test

--- compile.cmd
+++ compile.cmd
@@ -1,1 +1,1 @@
-javac -d bin src/is/hi/sbb51/hbv202g/ass3/backend/*.java src/is/hi/sbb51/hbv202g/ass3/frontend/*.java src/*.java
+javac -d bin src/*.java src/is/hi/sbb51/hbv202g/ass3/backend/*.java src/is/hi/sbb51/hbv202g/ass3/frontend/*.java
